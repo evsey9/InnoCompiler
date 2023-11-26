c033aac0c7e9c3bee86d34faba2b03a1e7684e38
evsey9 <evseyru1@mail.ru>
2023-11-26 20:49:41 +0300

    added additional node types

---
+++ LexicalAnalyzer/AstNodeOther.cs
@@ -0,0 +1,75 @@
+﻿using System.Collections.Generic;
+
+namespace LexicalAnalyzer
+{
+    public class StringNode : AstNode
+    {
+        public string StringValue;
+
+        public StringNode(string stringValue)
+        {
+            StringValue = stringValue;
+        }
+
+        public string GetString()
+        {
+            return StringValue;
+        }
+
+        public void SetString(string newString)
+        {
+            StringValue = newString;
+        }
+    }
+
+    public class ListNode<T> : AstNode
+    {
+        public List<T> NodeList = new List<T>();
+        
+        public ListNode()
+        {
+            NodeList = new List<T>();
+        }
+        
+        public ListNode(T node)
+        {
+            NodeList.Add(node);
+        }
+        
+        public ListNode(List<T> nodeList)
+        {
+            NodeList = nodeList;
+        }
+
+        public List<T> GetList()
+        {
+            return NodeList;
+        }
+
+        public void Add(T node)
+        {
+            NodeList.Add(node);
+        }
+    }
+
+    public class ExpressionNodeListNode : ListNode<ExpressionNode>
+    {
+    };
+
+    public class StringNodeListNode : ListNode<StringNode>
+    {
+    };
+
+    public class TupleNodeListNode : List<TupleElementNode>
+    {
+    };
+    
+    public class DeclarationNodeListNode : List<DeclarationNode>
+    {
+    };
+    
+    public class StatementNodeListNode : List<StatementNode>
+    {
+    };
+
+}
