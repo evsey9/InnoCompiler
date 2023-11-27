c0ec07a32ac69b0603000c3d492fe2e0302a69b0
Hallejujah123 <zhukov-02@yandex.ru>
2023-11-27 17:08:39 +0300

    No conflicts I hope

--- LexicalAnalyzer/AstNode.cs
+++ LexicalAnalyzer/AstNode.cs
@@ -3,7 +3,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
-
+//just some comment so github can upload it
 namespace LexicalAnalyzer
 {
     public abstract class AstNode { }
